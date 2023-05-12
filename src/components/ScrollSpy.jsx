6896860ecffdb72423b1eab3ee20b4c7d37139a3
Jaime Sánchez <jsanchez@surcontrol.com>
2023-05-13 00:38:03 +0200

    WIP (development): Añadidas posiciones de los puntos del scrollspy.

--- src/components/ScrollSpy.jsx
+++ src/components/ScrollSpy.jsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
+
 export default function ScrollSpy() {
+
+    
+    useEffect(() => {
+        const sections = document.querySelectorAll("main > .main-content > section");
+        const liElements = document.querySelectorAll(".ScrollSpy > ul > li");
+                
+        sections.forEach((section, i) => {
+            const { bottom } = section.getBoundingClientRect();
+            const sectionHeight = section.offsetHeight;
+
+            const sectionBallPosition = bottom - sectionHeight; // buscar otra manera de calcularlo
+            liElements[i].style.top = sectionBallPosition.toString() + "px";
+        })
+
+    }, [])
 
     return (
         <div className="ScrollSpy">
-            
+            <ul>
+                <li>
+                    <span>Some of my side projects</span>
+                </li>
+                <li>
+                    <span>Other section</span>
+                </li>
+            </ul>
         </div>
     )
 }
